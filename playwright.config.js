8aafc4531d126c6f4879bbc54d6d9e06b1bba38c
Yukhnov <romanyuhnov@yandex.ru>
2025-07-07 22:24:50 +0300

    test: add report

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { open: "never" }],
+    ["line"],
+    ["allure-playwright", {
+      resultsDir: "allure-results",
+    }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://academybugs.com/find-bugs/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
